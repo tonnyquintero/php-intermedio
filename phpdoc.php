925993351ef68e8d5228a56a32d0cf20b0e56f90
tonny187 <tonyoanthony@gmail.com>
2024-02-06 19:01:46 -0500

    first file

---
+++ phpdoc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * una funcion de sumar
+ *
+ * @param int $n1
+ * @param int $n2
+ * @return int devuelve el valor de la suma
+ */
+
+function add($n1, $n2) {
+    return $n1 + $n2;
+}
+
+$resultado = add(3, 4);
+
+echo $resultado;
+echo "\n";
