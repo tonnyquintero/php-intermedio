79a4148641c97be8b9631a6a0aadb7c88f032b57
tonny187 <tonyoanthony@gmail.com>
2024-03-05 19:22:24 -0500

    fixing scope file

--- scope.php
+++ scope.php
@@ -3,7 +3,7 @@
 $outside_variable = "Esto es una variable global";
 
 function my_function() {
-# We need to put the word global so that the function can be accessed 
+# We need to put the word global so that the function can be accessed
     global $outside_variable;
     echo $outside_variable;
 }
