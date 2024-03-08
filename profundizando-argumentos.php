e6f92a9e0d1e20b1cc552bbc0f59fb2214f5bc9d
tonny187 <tonyoanthony@gmail.com>
2024-03-08 06:20:59 -0500

    adding arguments functionalitys

---
+++ profundizando-argumentos.php
@@ -0,0 +1,33 @@
+<?php
+function sumar_edades($edades=array(13,14,35)){
+    return array_sum($edades);
+}
+
+echo sumar_edades();
+
+echo "\n";
+
+
+function multiplicar(   $n1=1,
+ $n2=2,
+ $n3=3,){
+    return $n1*$n2*$n3;     
+ }
+
+ echo multiplicar();
+ echo "\n";
+
+
+//Incorrecto
+/* function suma($n1=2,$n2){
+    return $n1 + $n2;
+} */
+//Correcto
+function suma($n2,$n1=2){
+    return $n1 + $n2;
+} 
+
+echo suma(8,7);
+echo "\n";
+
+?>
