e659d163825f1f5577bfb7a108d7b17a840b4558
tonny187 <tonyoanthony@gmail.com>
2024-03-08 19:32:50 -0500

    adding arrow function file

---
+++ arrow-functions.php
@@ -0,0 +1,23 @@
+<?php
+
+$cajero = 1;
+
+$add_cajero = fn($add) => $cajero + $add;
+
+echo $add_cajero(20);
+
+echo "\n";
+
+$edades = [5, 38, 17, 20, 9, 50];
+
+// Without arrow functions 
+// $mayores_de_edad = array_filter($edades, function($current) {
+//     return $current >= 18;
+// });
+
+$mayores_de_edad = array_filter($edades, fn($current) => $current >= 18);
+
+
+
+print_r($mayores_de_edad);
+echo "\n";
